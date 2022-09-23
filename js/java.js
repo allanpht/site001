99f3a34ea44ef81330ee70aae7526faf5dec7279
allanpht <allan.lc@icloud.com>
2022-09-23 15:10:48 -0300

    ajuste pasta js

--- js/java.js
+++
@@ -1,9 +0,0 @@
-var offset = $('#meuMenu').offset().top;
-var $meuMenu = $('#meuMenu'); // guardar o elemento na memoria para melhorar performance
-$(document).on('scroll', function () {
-    if (offset <= $(window).scrollTop()) {
-        $meuMenu.addClass('fixar');
-    } else {
-        $meuMenu.removeClass('fixar');
-    }
-});
